0c118a38dfb739955cbe5697a0dbf5865f4fc496
José Isaac <joseisaacnascimento@gmail.com>
2025-10-23 18:02:01 -0300

    feat(hero): criar seção hero com icon e dois buttons placeholders

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { ClipboardCheck } from "lucide-react";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <section className="h-screen flex flex-col items-center justify-center text-center gap-6">
+      {/* img */}
+      <ClipboardCheck size={100} />
+      {/* title */}
+      <h1 className="text-lg max-w-2xl md:text-3xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        blanditiis omnis rem enim!
+      </h1>
+      {/* div btn */}
+      <div className="px-4 flex flex-col md:flex-row w-full gap-4 items-center justify-center">
+        <button className="btn w-full md:max-w-[200px]">lorem</button>
+        <button className="btn w-full md:max-w-[200px]">lorem</button>
+      </div>
+    </section>
   );
 }
