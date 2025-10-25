b5aaf91ef9251e436c5e022f57bee8d6395ac7cf
José Isaac <joseisaacnascimento@gmail.com>
2025-10-25 15:20:49 -0300

    fix(header): mudar extensão do header para tsx e adicionar lógica para saber se está logado

---
+++ app/components/Header.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+// icon
+import { PlusCircle } from "lucide-react";
+
+// link
+import Link from "next/link";
+
+// session e auth
+import { useSession, signIn, signOut } from "next-auth/react";
+
+// hooks
+export default function Header() {
+  const { data: session, status } = useSession();
+  return (
+    // header
+    <header className="flex flex-row items-center justify-around py-2">
+      {/* nav */}
+      <nav className="flex flex-row items-center gap-2">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="flex items-center gap-1 text-2xl font-bold">
+            Tarefas <PlusCircle className="text-red-600" />
+          </h1>
+        </Link>
+        {/* Btn para dash */}
+        {/* se tiver sessao, mostrar o botão, se não, não mostrar nada */}
+        {session && (
+          <Link
+            href="/dashboard"
+            className="hover text-sm rounded-4xl border border-white px-4 py-1 font-bold"
+          >
+            Meu painel
+          </Link>
+        )}
+      </nav>
+
+      {/* Btn entrar / sair */}
+
+      {/* se o status for loading, não mostrar nada, se tiver uma sessao, mostrar o botão de sair, se não, mostrar o botão de entrar */}
+      {status === "loading" ? (
+        <></>
+      ) : session?.expires ? (
+        <button
+          className="hover text-sm rounded-4xl border border-white px-2 md:px-4 py-1 font-bold"
+          onClick={() => signOut()}
+        >
+          Olá, {session?.user?.name}
+        </button>
+      ) : (
+        <button
+          className="hover text-sm rounded-4xl border border-white px-4 py-1 font-bold"
+          onClick={() => signIn("google")}
+        >
+          Entrar
+        </button>
+      )}
+    </header>
+  );
+}
