4eb2de514f0ca010c04370fd215993111ca16ed5
José Isaac <joseisaacnascimento@gmail.com>
2025-10-25 15:18:36 -0300

    feat(dashboard): criar page dashboard com apenas um h1

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+// import head
+import Head from "next/head";
+
+export default function Dashboard() {
+  return (
+    <section>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
+      <h1>Painel</h1>
+    </section>
+  );
+}
