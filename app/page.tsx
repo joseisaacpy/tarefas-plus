94b670abdd198c45d1e5a42461f8a90806e2c6a9
José Isaac <joseisaacnascimento@gmail.com>
2025-10-27 15:20:47 -0300

    feat: adicionar textos sobre o sistema de tarefas na página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { ClipboardCheck } from "lucide-react";
 
 export const metadata = {
-  title: "Início",
+  title: "Tarefas Plus",
 };
 
 export default function Home() {
@@ -10,14 +10,18 @@ export default function Home() {
       {/* img */}
       <ClipboardCheck size={100} />
       {/* title */}
+      {/* título */}
       <h1 className="text-lg max-w-2xl md:text-3xl font-bold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-        blanditiis omnis rem enim!
+        Organize suas tarefas de forma simples e eficiente. Crie, edite e
+        acompanhe seu progresso com praticidade.
       </h1>
-      {/* div btn */}
+
+      {/* botões */}
       <div className="px-4 flex flex-col md:flex-row w-full gap-4 items-center justify-center">
-        <button className="btn w-full md:max-w-[200px]">lorem</button>
-        <button className="btn w-full md:max-w-[200px]">lorem</button>
+        <button className="btn w-full md:max-w-[200px]">+ 12 posts</button>
+        <button className="btn w-full md:max-w-[200px] btn-outline">
+          + 12 comentários
+        </button>
       </div>
     </section>
   );
