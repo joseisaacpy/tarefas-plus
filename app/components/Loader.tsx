943eba0957e65f779f1956f5883ac43ddc125ba6
José Isaac <joseisaacnascimento@gmail.com>
2025-10-27 15:19:23 -0300

    feat: criar componente de loader

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="animate-spin h-5 w-5 border-b-2 border-white rounded-full" />
+      <div className="animate-spin h-10 w-10 border-b-2 border-white rounded-full" />
     </div>
   );
 }
