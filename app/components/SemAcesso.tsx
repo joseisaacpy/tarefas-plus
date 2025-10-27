5667c04c3c2ddadfbdf89cbf3ab395bc5a3dd096
José Isaac <joseisaacnascimento@gmail.com>
2025-10-27 15:16:52 -0300

    feat: criar página de sem acesso

---
+++ app/components/SemAcesso.tsx
@@ -0,0 +1,9 @@
+export default function SemAcesso() {
+  return (
+    <section className="h-[calc(100dvh-100px)] p-2 flex items-center justify-center">
+      <h1 className="text-center text-3xl max-w-2xl font-bold">
+        Você não tem acesso a essa página. Por favor, faça o login primeiro.
+      </h1>
+    </section>
+  );
+}
