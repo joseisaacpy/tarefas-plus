8c46584d1e706dcdc1fab63354ca30498be5c4ec
José Isaac <joseisaacnascimento@gmail.com>
2025-10-27 15:18:16 -0300

    refactor: separar autenticacao em lib - deixar responsibilidades separadas

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,7 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+// import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "@/app/lib/auth";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.JWT_SECRET as string,
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
